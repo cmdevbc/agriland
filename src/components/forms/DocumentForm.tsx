feace41a9faa2a0f0accc528e414bad220f22f39
cemleme <cemadil@gmail.com>
2024-03-26 22:41:02 +0300

    add form email

--- src/components/forms/DocumentForm.tsx
+++ src/components/forms/DocumentForm.tsx
@@ -1,56 +1,83 @@
-"use client"
-import { toast } from 'react-toastify';
+"use client";
+import { toast } from "react-toastify";
 
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm as useFormSpree, ValidationError } from "@formspree/react";
+import { useEffect } from "react";
 
 interface FormData {
-   name: string;
-   email: string;
-   message: string;
+  name: string;
+  email: string;
+  message: string;
 }
 
 const schema = yup
-   .object({
-      name: yup.string().required().label("Name"),
-      email: yup.string().required().email().label("Email"),
-      message: yup.string().required().label("Message"),
-   })
-   .required();
+  .object({
+    name: yup.string().required().label("Name"),
+    email: yup.string().required().email().label("Email"),
+    message: yup.string().required().label("Message"),
+  })
+  .required();
 
 const DocumentForm = () => {
+  const [state, handleSubmitSpree] = useFormSpree("xrgnybyj");
+  // const { register, handleSubmit, reset, formState: { errors }, } = useForm<FormData>({ resolver: yupResolver(schema), });
+  // const onSubmit = (data: FormData) => {
+  //    handleSubmitSpree(data);
+  //    const notify = () => toast('Message sent successfully', { position: 'top-center' });
+  //    notify();
+  //    reset();
+  // };
 
-   const { register, handleSubmit, reset, formState: { errors }, } = useForm<FormData>({ resolver: yupResolver(schema), });
-   const onSubmit = (data: FormData) => {
-      const notify = () => toast('Message sent successfully', { position: 'top-center' });
-      notify();
-      reset();
-   };
+  useEffect(() => {
+    if (state.succeeded) {
+      toast("Message sent successfully", { position: "top-center" });
+    }
+  }, [state]);
 
-   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-         <div className="row">
-            <div className="col-md-6">
-               <div className="form-grp">
-                  <input type="text" {...register("name")} placeholder="Your Name" />
-                  <p className="form_error">{errors.name?.message}</p>
-               </div>
-            </div>
-            <div className="col-md-6">
-               <div className="form-grp">
-                  <input type="email" {...register("email")} placeholder="Your Email" />
-                  <p className="form_error">{errors.email?.message}</p>
-               </div>
-            </div>
-         </div>
-         <div className="form-grp">
-            <textarea {...register("message")} placeholder="Enter you message....."></textarea>
-            <p className="form_error">{errors.message?.message}</p>
-         </div>
-         <button type="submit" className="btn">Send Message</button>
-      </form>
-   )
-}
+  return (
+    <form onSubmit={handleSubmitSpree}>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-grp">
+            <input type="text" name="name" id="name" placeholder="Your Name" />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form-grp">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="form-grp">
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Enter you message....."
+        ></textarea>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <button type="submit" className="btn">
+        Send Message
+      </button>
+    </form>
+  );
+};
 
-export default DocumentForm
+export default DocumentForm;
