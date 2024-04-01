26dedf0ba9549d9db9c9971a6b57a0fe86a20910
sfwdv <sfwdv@git.com>
2024-04-01 17:48:03 +0330

    change contract address

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,8 +1,8 @@
-export const contractAddress = "0x15a053Bd881E966850b0fB6B5aA292d5fEd7f06C";
+export const contractAddress = "0x2b320eedcf8558c080c954dd0d6263435e267105";
 export const usdtContractAddress = "0x55d398326f99059fF775485246999027B3197955";
 export const chainId = 56;
 export const blockExplorer = "https://bscscan.com/tx/";
 export const token = {
-  address: "0xf61a4Dc597D275c104A8A536D5f6F859881A3dBe",
+  address: "0x243dAf44CC1796c9910775d722a542E3e36F258D",
   symbol: "ALT",
 };
